f50e8aa624db93a1ffa29ee3d36a77ddbfab3617
shuvro18 <shuvrobiswas404@gmail.com>
2026-06-13 23:56:37 +0600

    footer section added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,8 @@ export default function RootLayout({ children }) {
         <main>
           {children}
         </main>
+
+        <Footer></Footer>
 
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <p className="text-primary font-bold text-4xl"> hello this is main page</p>
+      <p className="text-primary font-bold text-4xl min-h-screen  flex items-center justify-center"> hello this is main page</p>
     </div>
   );
 }
